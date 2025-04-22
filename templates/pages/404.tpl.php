09a25c87ad240cddd17e18f54b11f7fe0cd5183a
becskerekisandor <becskereki.sandor@nhsz.hu>
2025-04-22 10:32:23 +0200

    Add files via upload

---
+++ templates/pages/404.tpl.php
@@ -0,0 +1,5 @@
+<div class="alert alert-danger">
+    <h2>404 - Az oldal nem található</h2>
+    <p>A keresett oldal nem található. Kérjük, ellenőrizze a megadott URL-t vagy térjen vissza a főoldalra.</p>
+    <a href="./" class="btn btn-primary">Vissza a főoldalra</a>
+</div>
